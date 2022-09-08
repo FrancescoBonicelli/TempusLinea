c4f15b1e75cd06d9cec41bc4a567e88c164b558a
Stefano Bonicelli <stebonicelli@gmail.com>
2022-09-08 21:38:37 +0200

    changed v_offset logic

--- TempusLinea/canvas.cpp
+++ TempusLinea/canvas.cpp
@@ -7,17 +7,17 @@ Canvas::Canvas(QWidget *parent) : QWidget{parent}
     //Setup default parameters
     range_begin = 1950 * 12 * 31;
     range_end = 2050 * 12 * 31;
+
+    v_offset = 0;
 }
 
 void Canvas::paintEvent(QPaintEvent *)
 {
-    v_offset = height() / 2;
-
     QPainter painter(this);
 
     QColor timeline_color(0, 0,0);
 
     // Paint Timeline
     painter.setPen(timeline_color);
-    painter.drawLine(0, v_offset, width(), v_offset);
+    painter.drawLine(0, (height() / 2) + v_offset, width(), (height() / 2) + v_offset);
 }
