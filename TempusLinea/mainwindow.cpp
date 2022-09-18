477b0bbb2be8a63e99045b1a2d989496f6de29e5
Stefano Bonicelli <stebonicelli@gmail.com>
2022-09-18 22:18:07 +0200

    fixed "cancel" option in canvas save

--- TempusLinea/mainwindow.cpp
+++ TempusLinea/mainwindow.cpp
@@ -59,7 +59,9 @@ bool MainWindow::saveCanvas(QString file_name)
 void MainWindow::saveCanvasSlot()
 {
     QString file_name = QFileDialog::getSaveFileName(0, "Save Canvas");
-    saveCanvas(file_name);
+
+    if(!file_name.isEmpty())
+        saveCanvas(file_name);
 }
 
 void MainWindow::loadCanvasSlot()
